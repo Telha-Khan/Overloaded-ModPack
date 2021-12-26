d6b1d0cfdd13b3463b354293c82ec5b1fa42e250
itsyaboyjack <72960618+itsyaboyjack@users.noreply.github.com>
2021-12-26 11:17:32 -0500

    initial 2

--- Mod Setup/src/main/java/com/overloaded/overloadedmod/ModItems.java
+++ Mod Setup/src/main/java/com/overloaded/overloadedmod/ModItems.java
@@ -9,11 +9,13 @@ public class ModItems {
     public static void init() {
 
         //HellStone
-        Item hellstone_gem = new Item().setUnlocalizedName("hellstone_gem").setTextureName(OverloadedMod.MODID + ":hellstone_gem").setCreativeTab(CreativeTabs.tabMaterials);
-        GameRegistry.registerItem(hellstone_gem, hellstone_gem.getUnlocalizedName());
+        Item hellstone = new Item().setUnlocalizedName("hellstone").setTextureName(OverloadedMod.MODID + ":hellstone").setCreativeTab(CreativeTabs.tabMaterials);
+        GameRegistry.registerItem(hellstone, hellstone.getUnlocalizedName());
         
         Item hellstone_dust = new Item().setUnlocalizedName("hellstone_dust").setTextureName(OverloadedMod.MODID + ":hellstone_dust").setCreativeTab(CreativeTabs.tabMaterials);
         GameRegistry.registerItem(hellstone_dust, hellstone_dust.getUnlocalizedName());
+
+        
 
     }
 
